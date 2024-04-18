46b54107daa5250f37f460fb3eefd5c94393f71c
Wdson Danrlei <44691537+wdson91@users.noreply.github.com>
2024-04-18 11:28:47 -0300

    tio_orlando_teste

--- testehopperplus.py
+++ testehopperplus.py
@@ -17,14 +17,19 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
     
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 # Abrir a página desejada
-driver.get("https://disneyworld.disney.go.com/pt-br/admission/tickets/theme-parks/")
+driver.get("https://www.tioorlando.com.br/ingressos-disney-orlando")
 wait = WebDriverWait(driver, 5)
 # Aguardar até que o elemento radio-1 esteja presente na página
 # Aguardar até que o elemento radio-1 esteja presente na página
-element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div[2]/div/tickets-spa//div/div[2]/uirouter-uiview/tickets-config-page-v2//div[2]/tickets-module[1]/tickets-num-days//iron-selector/div[1]")))
+element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="page-content"]/div[2]/div/div[2]/div/div/div/div[2]/div/div[2]/div/div[1]/button')))
 
 # Clicar no elemento
 element.click()
 
+
+data = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="page-content"]/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/div[1]/div[1]')))
+print(data.text)
+
+time.sleep(50)
 # Fechar o navegador
 driver.quit()
