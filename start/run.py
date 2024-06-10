125d4a057eddc8a05cda9ed0c86ac40bfcbb491f
Wdson Danrlei <wdson91@gmail.com>
2024-06-10 07:03:36 -0300

    att

--- start/run.py
+++ start/run.py
@@ -8,34 +8,34 @@ from voupra.orlando.index_voupra import main_voupra  # Importa os módulos neces
 array_datas =  [5,10,20,47,65,126]
 
 async def executar_ambos(hour, array_datas, data_atual):
-    try:
-        await main_voupra(hour, array_datas, data_atual)  # Executa a função main_voupra com o argumento hour
-        logging.info("main_voupra concluída.")  # Registra uma mensagem de log
-    except Exception as e:
-        logging.error(f"Erro ao executar main_voupra: {e}")  # Registra uma mensagem de log de erro
-    
-    try:
-        await main_vmz(hour, array_datas, data_atual)  # Executa a função main_vmz com o argumento hour
-        logging.info("main_vmz concluída.")  # Registra uma mensagem de log
-    except Exception as e:
-        logging.error(f"Erro ao executar main_vmz: {e}")  # Registra uma mensagem de log de erro
-    
-    try:
-        await main_ml(hour, array_datas, data_atual)  # Executa a função main_ml com o argumento hour
-        logging.info("main_ml concluída.")  # Registra uma mensagem de log
-    except Exception as e:
-        logging.error(f"Erro ao executar main_ml: {e}")  # Registra uma mensagem de log de erro
-
     # try:
-    #         empresas = ['voupra', 'vmz', 'decolar','ml','tio','fastPass']
-    #         parques = ['disney', 'universal', 'seaworld']
-            
-    #         juntar_json = JuntarJsons(data_atual, empresas, parques, 'orlando')
-            
-    #         await juntar_json.executar()
-    
+    #     await main_voupra(hour, array_datas, data_atual)  # Executa a função main_voupra com o argumento hour
+    #     logging.info("main_voupra concluída.")  # Registra uma mensagem de log
     # except Exception as e:
-    #         logging.error(f"Erro durante a junção dos arquivos: {e}")
+    #     logging.error(f"Erro ao executar main_voupra: {e}")  # Registra uma mensagem de log de erro
+    
+    # try:
+    #     await main_vmz(hour, array_datas, data_atual)  # Executa a função main_vmz com o argumento hour
+    #     logging.info("main_vmz concluída.")  # Registra uma mensagem de log
+    # except Exception as e:
+    #     logging.error(f"Erro ao executar main_vmz: {e}")  # Registra uma mensagem de log de erro
+    
+    # try:
+    #     await main_ml(hour, array_datas, data_atual)  # Executa a função main_ml com o argumento hour
+    #     logging.info("main_ml concluída.")  # Registra uma mensagem de log
+    # except Exception as e:
+    #     logging.error(f"Erro ao executar main_ml: {e}")  # Registra uma mensagem de log de erro
+
+    try:
+            empresas = ['voupra', 'vmz', 'decolar','ml','tio','fastPass']
+            parques = ['disney', 'universal', 'seaworld']
+            
+            juntar_json = JuntarJsons(data_atual, empresas, parques, 'orlando')
+            
+            await juntar_json.executar()
+    
+    except Exception as e:
+            logging.error(f"Erro durante a junção dos arquivos: {e}")
 
 # Verifica se o script está sendo executado diretamente
 if __name__ == "__main__":
