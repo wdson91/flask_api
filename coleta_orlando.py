a907d917a5aa3053c71ab966b7e3347ac31f9835
Wdson Danrlei <wdson91@gmail.com>
2024-06-18 14:42:06 -0300

    Update coleta_orlando.py

--- coleta_orlando.py
+++ coleta_orlando.py
@@ -9,10 +9,11 @@ dias_para_processar = [2,3,4,5]
 
 async def bloco01(hora_global,array_datas,data_atual):
 
-        # try:
-        #     await coletar_precos_vmz_disneybasicos(array_datas,hora_global,data_atual)
+        try:
+            await coletar_precos_vmz_disneybasicos(array_datas,hora_global,data_atual)
             
-        # except Exception as e:
+        except Exception as e:
+            logging.error(f"Erro durante a coleta de preços da Disney: {e}")
         try:
             await coletar_precos_vmz_universal(hora_global,array_datas,data_atual)
             
